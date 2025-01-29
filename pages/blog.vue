5fbd63babef4ab862c97de6c0784129ef941bb41
AlexMel83 <meleshko.a.n@ukr.net>
2025-01-29 08:44:57 +0200

    +

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,3 +1,5 @@
 <template>
-  <div></div>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <p class="text-xl font-semibold text-gray-700">Сторінка в розробці</p>
+  </div>
 </template>
