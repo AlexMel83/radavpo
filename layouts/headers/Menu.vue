a8dff2bbaa76e14c5a39a123dfbfc74d3afd641a
AlexMel83 <meleshko.a.n@ukr.net>
2025-01-29 13:44:30 +0200

    +

--- layouts/headers/Menu.vue
+++ layouts/headers/Menu.vue
@@ -1,120 +1,104 @@
 <template>
   <div class="menu-group">
-    <ul class="menu-list">
-      <li>
-        <NuxtLink class="hover:text-primary-300" to="/">
+    <ul class="flex justify-around p-0 m-0 list-none">
+      <li class="relative mx-5">
+        <NuxtLink
+          class="hover:text-primary-300 p-1 inline-block transition-colors border-b-2 border-transparent hover:border-primary-300"
+          to="/"
+        >
           {{ $t('menu.home') }}
         </NuxtLink>
       </li>
-      <li>
-        <NuxtLink class="hover:text-primary-300" to="/about">
+      <li
+        class="relative mx-5 flex items-center group"
+        @mouseenter="isMenuAboutOpen = true"
+        @mouseleave="isMenuAboutOpen = false"
+      >
+        <NuxtLink
+          class="hover:text-primary-300 p-1 inline-block transition-colors border-b-2 border-transparent group-hover:border-primary-300"
+          to="/about"
+        >
           {{ $t('menu.about') }}
         </NuxtLink>
-        <ul class="submenu">
+        <ul
+          class="absolute right-3 top-full hidden bg-white dark:bg-gray-800 shadow-md rounded-md p-1 group-hover:block transition-all duration-300"
+        >
           <li>
-            <NuxtLink class="hover:text-primary-300" to="/reporting">
+            <NuxtLink
+              class="block p-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-primary-300 transition-colors border-b-2 border-transparent hover:border-primary-300"
+              to="/reporting"
+            >
               {{ $t('menu.reporting') }}
             </NuxtLink>
           </li>
         </ul>
+        <CollapsedIcon v-if="!isMenuAboutOpen" />
+        <ExpandedIcon class="text-primary-300" v-if="isMenuAboutOpen" />
       </li>
-      <li>
-        <NuxtLink class="hover:text-primary-300" to="/blog">
+
+      <li class="relative mx-5">
+        <NuxtLink
+          class="hover:text-primary-300 p-1 inline-block transition-colors border-b-2 border-transparent hover:border-primary-300"
+          to="/blog"
+        >
           {{ $t('menu.blog') }}
         </NuxtLink>
       </li>
-      <li>
-        <NuxtLink class="hover:text-primary-300" to="/resources">
+      <li
+        class="relative mx-5 flex items-center group"
+        @mouseenter="isMenuResourcesOpen = true"
+        @mouseleave="isMenuResourcesOpen = false"
+      >
+        <NuxtLink
+          class="hover:text-primary-300 p-1 inline-block transition-colors border-b-2 border-transparent group-hover:border-primary-300"
+          to="/resources"
+        >
           {{ $t('menu.resources') }}
         </NuxtLink>
-        <!-- <ul class="submenu">
+        <ul
+          class="absolute right-3 top-full hidden bg-white dark:bg-gray-800 shadow-md rounded-md p-1 group-hover:block transition-all duration-300"
+        >
           <li>
-            <NuxtLink to="/housing">{{ $t('menu.housing') }}</NuxtLink>
+            <NuxtLink
+              class="block p-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-primary-300 transition-colors border-b-2 border-transparent hover:border-primary-300"
+              to="/housing"
+              >{{ $t('menu.housing') }}</NuxtLink
+            >
           </li>
           <li>
-            <NuxtLink to="/employment">{{ $t('menu.employment') }}</NuxtLink>
+            <NuxtLink
+              class="block p-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-primary-300 transition-colors border-b-2 border-transparent hover:border-primary-300"
+              to="/employment"
+              >{{ $t('menu.employment') }}</NuxtLink
+            >
           </li>
           <li>
-            <NuxtLink to="/support">{{ $t('menu.support') }}</NuxtLink>
+            <NuxtLink
+              class="block p-1 hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-primary-300 transition-colors border-b-2 border-transparent hover:border-primary-300"
+              to="/support"
+              >{{ $t('menu.support') }}</NuxtLink
+            >
           </li>
-        </ul> -->
+        </ul>
+        <CollapsedIcon v-if="!isMenuResourcesOpen" />
+        <ExpandedIcon class="text-primary-300" v-if="isMenuResourcesOpen" />
       </li>
-      <li>
-        <NuxtLink class="hover:text-primary-300" to="/join">
+      <li class="relative mx-5">
+        <NuxtLink
+          class="hover:text-primary-300 p-1 inline-block transition-colors border-b-2 border-transparent hover:border-primary-300"
+          to="/join"
+        >
           {{ $t('menu.join') }}
         </NuxtLink>
-        <!-- <ul class="submenu">
-          <li>
-            <NuxtLink to="/donate">{{ $t('menu.donate') }}</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/events">{{ $t('menu.events') }}</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/volunteer">{{ $t('menu.volunteer') }}</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/newsletter">{{ $t('menu.newsletter') }}</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/collaboration">{{
-              $t('menu.collaboration')
-            }}</NuxtLink>
-          </li>
-        </ul> -->
       </li>
     </ul>
   </div>
 </template>
 
 <script setup>
-// Здесь можно добавить пропсы, если необходимо
+import CollapsedIcon from '~/components/icons/CollapsedIcon.vue';
+import ExpandedIcon from '~/components/icons/ExpandedIcon.vue';
+
+const isMenuAboutOpen = ref(false);
+const isMenuResourcesOpen = ref(false);
 </script>
-
-<style scoped>
-.menu-list {
-  display: flex;
-  justify-content: space-around;
-  padding: 0;
-  margin: 0;
-  list-style-type: none;
-}
-
-.menu-list > li {
-  position: relative;
-  margin: 0 20px;
-}
-
-.menu-list a {
-  text-decoration: none;
-  /* color: var(--text-color); */
-  padding: 10px 15px;
-  display: inline-block;
-  transition:
-    color 0.3s ease,
-    background-color 0.3s ease;
-}
-
-.submenu {
-  display: none;
-  position: absolute;
-  top: 100%;
-  left: 0;
-  padding: 0;
-  margin: 0;
-  list-style-type: none;
-}
-
-.menu-list > li:hover .submenu {
-  display: block;
-}
-
-.submenu li {
-  margin: 0;
-}
-
-.submenu a {
-  padding: 0px 20px;
-  white-space: nowrap;
-}
-</style>
---
+++ pages/support.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <p class="text-xl font-semibold text-gray-700">Сторінка в розробці</p>
+  </div>
+</template>
