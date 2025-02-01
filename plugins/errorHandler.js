e0f3345b123b4a49ea955617193a12a6290c18b8
AlexMel83 <meleshko.a.n@ukr.net>
2025-02-01 15:31:04 +0200

    +

--- plugins/errorHandler.js
+++ plugins/errorHandler.js
@@ -6,8 +6,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     } catch (error) {
       if (errHandler) {
         if (error.response) {
-          const message =
-            error.response?.data?.message || 'Unknown error occurred';
+          const message = error.response?.data?.message || 'Unknown error occurred';
           if (message.includes('Невірний пароль')) {
             errHandler.password = 'Невірний пароль';
           } else if (message.includes('mail-server')) {
@@ -15,24 +14,20 @@ export default defineNuxtPlugin((nuxtApp) => {
           } else if (message.includes('вже існує')) {
             errHandler.email = 'Ця пошта вже зареєстрована';
           } else if (message.includes('не знайдений')) {
-            errHandler.email =
-              'Цей email не зареєстровано, будь ласка зареєструйтесь';
+            errHandler.email = 'Цей email не зареєстровано, будь ласка зареєструйтесь';
           } else if (message.includes('Validation error')) {
             errHandler.email = 'Перевірте правильність введених даних.';
           } else if (message.includes('535 Incorrect authentication data')) {
-            errHandler.email =
-              'Помилка відправки листа активації. Зверніться до адміністратора';
+            errHandler.email = 'Помилка відправки листа активації. Зверніться до адміністратора';
           } else if (message.includes('не активовано')) {
-            errHandler.email =
-              'Обліковий запис не активовано. Перевірте пошту або зверніться до адміністратора.';
+            errHandler.email = 'Обліковий запис не активовано. Перевірте пошту або зверніться до адміністратора.';
           } else {
             errHandler.form = message;
           }
         } else if (error.request) {
           console.log('Error', error);
           alert('Error: ' + JSON.stringify(error));
-          errHandler.textError = errHandler.email =
-            'Server is not responding. Please try again later.';
+          errHandler.textError = errHandler.email = 'Server is not responding. Please try again later.';
         } else {
           errHandler.textError = error.message || 'An unknown error occurred.';
         }
