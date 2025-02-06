63ba60935493e079110e6542a3289e0b9a09b87b
AlexMel83 <meleshko.a.n@ukr.net>
2025-02-06 16:33:58 +0200

    dsds

--- pages/page_under_construction.vue
+++ pages/page_under_construction.vue
@@ -3,7 +3,9 @@
     <p class="text-xl font-semibold text-gray-700">Сторінка в розробці</p>
   </div> -->
   <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    <p class="text-xl font-semibold text-gray-700">{{ alertMessage }}</p>
+    <p class="text-xl font-semibold text-gray-700">
+      {{ alertMessage }}
+    </p>
   </div>
 </template>
 
