0bf0df2fc3dca41c373c97caf8b93d09904830c2
Alexander Meleshko <meleshko.a.n@ukr.net>
2025-07-20 21:12:38 +0300

    +

--- server/api/partners/index.ts
+++
@@ -1,9 +0,0 @@
-import { readFile } from 'fs/promises';
-import { join } from 'path';
-
-export default defineEventHandler(async () => {
-  const filePath = join(process.cwd(), 'content', 'partners.json');
-  const data = await readFile(filePath, 'utf-8');
-  return JSON.parse(data);
-});
-// This code defines an event handler for a server API endpoint that reads a JSON file containing blog data.
